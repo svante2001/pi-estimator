3632215c85631555817871d0521c0b6d5cccf259
Svante Geisshirt <svante2001@gmail.com>
2020-05-04 10:29:02 +0200

    Initial commit

---
+++ pi-estimater/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace piestimater {
+	class MainClass {
+		public static void Main(string[] args) {
+			Random rng = new Random();
+
+			int points= 100000000;
+			int unit_circle = 0;
+
+			for (int i = 0; i < points;) {
+				double x = rng.NextDouble();
+				double y = rng.NextDouble();
+
+				if (x * x + y * y <= 1) {
+					unit_circle++;
+				}
+				i++;
+			}
+			double pi = 4.0 * unit_circle / points;
+			Console.WriteLine("PI er estimeret til: " + pi);
+		}
+	}
+}
